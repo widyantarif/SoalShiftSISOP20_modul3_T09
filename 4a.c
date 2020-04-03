6ecb0c740f3804ec7e7428a9feef49fe53e3e955
widyantarif <55181778+widyantarif@users.noreply.github.com>
2020-04-03 17:14:42 +0700

    Update 4a.c

--- 4a.c
+++ 4a.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
