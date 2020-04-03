36d1cd0b172058b0357150b8f7000b7085ea1a1a
widyantarif <55181778+widyantarif@users.noreply.github.com>
2020-04-03 17:15:14 +0700

    Update 4a.c

--- 4a.c
+++ 4a.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
-#include<sys/shm.h>
-#include<sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
